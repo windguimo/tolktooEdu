c46f64e5414c65ef50ba9086a4554760ac79dfe3
windguimo <wind_guizmo@hotmail.fr>
2017-02-17 16:15:20 +0100

    add category to products and implement filter by category

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,11 @@
 class ProductsController < ApplicationController
 
 	def index
+		if params[:category]
+		@products = Product.where(category: params[:category])
+		else
 		@products = Product.all
-	
+		end
 	end
 
 	def show
@@ -11,22 +14,41 @@ class ProductsController < ApplicationController
 	end
 
 	def new
+		@product=Product.new
 
 	end	
 
 	def create
+		@product=Product.new(product_params)
+		if @product.save
+		redirect_to products_path
+		else
+		render :new
+		end
 
 	end	
 
 	def edit
+		@product =Product.find(params[:id])
 
 	end	
 	def update
+		@product =Product.find(params[:id])
+		if @product.update(product_params)
+			redirect_to products_path
+		else
+			render :edit
+		end	
 
 	end	
 
 	def destroy
-
+		@product = Product.find(params[:id])
+		@product.destroy
+		redirect_to products_path
 	end	
 
+	def product_params
+		params.require(:product).permit(:name, :url, :tagline, :category)
+	end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
 	validates :name, presence: true, uniqueness:true
 	validates :url, presence: true, uniqueness: true
+	validates :category, inclusion: { in: %w(tech education design), message: "%{value} is not a valid category"}
 end
+
